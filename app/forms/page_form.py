c49e6e3b775480c5c2ab825fccf99ab6514a8f65
Max Yaswen <myaswen@gmail.com>
2022-11-23 10:48:54 -0800

    Add page validation form

---
+++ app/forms/page_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class NotebookForm(FlaskForm):
+    title = StringField("Name", validators=[DataRequired(), Length(max=50)])
+    content = TextAreaField("Name", validators=[Length(max=250000)])
