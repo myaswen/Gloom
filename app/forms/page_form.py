3b6126406b7fc18cc8fa07b7f43b6ddbed21623c
Max Yaswen <myaswen@gmail.com>
2022-11-23 10:52:20 -0800

    Add tag validation form

--- app/forms/page_form.py
+++ app/forms/page_form.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
-class NotebookForm(FlaskForm):
+class PageForm(FlaskForm):
     title = StringField("Name", validators=[DataRequired(), Length(max=50)])
     content = TextAreaField("Name", validators=[Length(max=250000)])
