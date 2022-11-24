4130bb4b2b49638585e8ec372e2d234637b339cd
Max Yaswen <myaswen@gmail.com>
2022-11-24 10:08:50 -0800

    Refactor auth error messages

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -25,7 +25,7 @@ def authenticate():
     """
     if current_user.is_authenticated:
         return current_user.to_dict()
-    return {'errors': ['Unauthorized']}
+    return { 'errors': { "Unauthorized": "User is not authenticated" } }, 401
 
 
 @auth_routes.route('/login', methods=['POST'])
@@ -42,7 +42,7 @@ def login():
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
         return user.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    return {'errors': form.errors}, 401
 
 
 @auth_routes.route('/logout')
@@ -51,7 +51,7 @@ def logout():
     Logs a user out
     """
     logout_user()
-    return {'message': 'User logged out'}
+    return {'status': 'User logged out'}
 
 
 @auth_routes.route('/signup', methods=['POST'])
@@ -80,7 +80,7 @@ def sign_up():
 
         return user.to_dict()
 
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    return {'errors': form.errors}, 401
 
 
 @auth_routes.route('/unauthorized')
@@ -88,4 +88,4 @@ def unauthorized():
     """
     Returns unauthorized JSON when flask-login authentication fails
     """
-    return {'errors': ['Unauthorized']}, 401
+    return { 'errors': { "Unauthorized": "User is not authenticated" } }, 401
