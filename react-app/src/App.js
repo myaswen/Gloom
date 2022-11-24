e92b61bd621beec5a5d3d32cdce73bb3efe3b076
Max Yaswen <myaswen@gmail.com>
2022-11-24 12:07:44 -0800

    Preliminary positional styling for splash page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
+// import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
+// import NavBar from './components/NavBar';
+// import ProtectedRoute from './components/auth/ProtectedRoute';
+// import UsersList from './components/UsersList';
+// import User from './components/User';
 import { authenticate } from './store/session';
+
+import LoginPage from './components/LoginPage/LoginPage';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -26,20 +28,20 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      {/* <NavBar /> */}
       <Switch>
         <Route path='/login' exact={true}>
-          <LoginForm />
+          <LoginPage />
         </Route>
         <Route path='/sign-up' exact={true}>
           <SignUpForm />
         </Route>
-        <ProtectedRoute path='/users' exact={true} >
+        {/* <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
-        </ProtectedRoute>
+        </ProtectedRoute> */}
         <Route path='/' exact={true} >
           <h1>My Home Page</h1>
         </Route>
---
+++ react-app/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { login } from '../../store/session';
+import "./LoginPage.css"
+
+
+const LoginPage = () => {
+    const [errors, setErrors] = useState([]);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const user = useSelector(state => state.session.user);
+    const dispatch = useDispatch();
+
+    const onLogin = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login(email, password));
+        if (data) {
+            setErrors(data);
+            console.log(errors);
+        }
+    };
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    if (user) {
+        return <Redirect to='/' />;
+    }
+
+    return (
+        <div id='login-page-wrapper'>
+            <div id="splash-info-wrapper">
+                <p>Placeholder for splash page info</p>
+            </div>
+            <div id="login-form-wrapper">
+                <form onSubmit={onLogin}>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            name='email'
+                            type='text'
+                            placeholder='Email'
+                            value={email}
+                            onChange={updateEmail}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            name='password'
+                            type='password'
+                            placeholder='Password'
+                            value={password}
+                            onChange={updatePassword}
+                        />
+                        <button type='submit'>Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
