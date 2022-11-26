86b2bd7c10b524b54f3694daf17bc47f12479f26
Max Yaswen <myaswen@gmail.com>
2022-11-26 15:44:54 -0800

    Page edit form setup

--- react-app/src/components/PageView/PageView.js
+++ react-app/src/components/PageView/PageView.js
@@ -1,32 +1,54 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import "./PageView.css";
 
 const PageView = () => {
+    // const dispatch = useDispatch();
     const { pageId } = useParams();
 
     const notebookPages = useSelector(state => state.pages.notebook);
     const currentPage = notebookPages[pageId];
 
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        setTitle(currentPage?.title || "");
+        setContent(currentPage?.content || "");
+    }, [currentPage]);
+
     const savedDate = new Date(currentPage?.updatedAt).toLocaleString();
 
     return (
         <div id="page-view-wrapper">
+
             <div id="page-view-header">
-                <div>{currentPage?.title}</div>
+                <input
+                id="page-edit-title-input"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
                 <div id="save-page-container">
-                    <div>Last save {savedDate}</div>
                     <div>Save</div>
+                    <div id="page-save-date">Last save {savedDate}</div>
                 </div>
             </div>
+
             <div id="page-view-body">
-                {currentPage?.content}
+                <textarea
+                id="page-edit-content-input"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                />
             </div>
+
             <div id="page-view-footer">
                 Page view footer placeholder
             </div>
+
         </div>
     )
 }
