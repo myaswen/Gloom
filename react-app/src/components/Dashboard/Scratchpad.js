32a9e32c198133313176d940f9c517edfb95f519
Max Yaswen <myaswen@gmail.com>
2022-11-27 11:44:59 -0800

    Connect get scratchpad thunk to comp

--- react-app/src/components/Dashboard/Scratchpad.js
+++ react-app/src/components/Dashboard/Scratchpad.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TH_getScratchpad } from "../../store/scratchpad";
 
 import "./Scratchpad.css";
 
 const Scratchpad = () => {
+    const dispatch = useDispatch();
+
+    const scratchpad = useSelector(state => state.scratchpad);
+
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        setContent(scratchpad.content);
+    }, [scratchpad]);
+
+    useEffect(() => {
+        dispatch(TH_getScratchpad());
+    }, [dispatch]);
+
+    console.log("CURRENT SCRATCHPAD: ", scratchpad);
     return (
         <div id="scratchpad-container">
             <div id="scratchpad-options-wrapper">
