a9fec946fdf44550f1b6552a2bfa9b3b8ff30dc1
Max Yaswen <myaswen@gmail.com>
2022-11-27 15:56:59 -0800

    Map recent pages in recent page comp

--- react-app/src/components/Dashboard/RecentPages.js
+++ react-app/src/components/Dashboard/RecentPages.js
@@ -1,18 +1,42 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { TH_getUserPages } from "../../store/page";
 
 import "./RecentPages.css";
 
 const RecentPages = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const userPages = useSelector(state => state.pages.user);
+
+    useEffect(() => {
+        dispatch(TH_getUserPages());
+    }, [dispatch]);
+
+    const pageIds = Object.keys(userPages);
+    const recentPageIds = pageIds.slice(pageIds.length - 10, pageIds.length);
+
+    const selectPage = (pageId) => {
+        history.push(`/notebooks/${userPages[pageId].notebookId}/pages/${pageId}`);
+    }
 
     return (
         <div id="recent-pages-wrapper">
 
             <div id="recent-pages-header">
-                <div>Recent pages</div>
+                <div>Recently created</div>
             </div>
 
             <div id="recent-pages-cards-wrapper">
-                recent page cards
+                {recentPageIds.map(pageId => (
+                    <div className="recent-page-card clickable" key={pageId} onClick={() => selectPage(pageId)}>
+                        <div>{userPages[pageId].title}</div>
+                        <div>{userPages[pageId].updatedAt}</div>
+                    </div>
+                ))}
             </div>
 
         </div>
