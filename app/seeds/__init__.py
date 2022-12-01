f50cc44b8ff18f8ee813e724a43fa6d3e08a6a13
Max Yaswen <myaswen@gmail.com>
2022-11-30 17:12:06 -0800

    Successful rich-text seed one

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 # from .users import seed_users, undo_users
 from .test_seeds import seed_test, undo_test
+from .demo_seeds import demo_seeds, undo_demo_seeds
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,13 +18,19 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_test()
-    seed_test()
+
+        # undo_test()
+        undo_demo_seeds()
+    # seed_test()
+    demo_seeds()
+
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_test()
+    # undo_test()
+    undo_demo_seeds()
+
     # Add other undo functions here
