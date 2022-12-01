5217d99be076fc1d58eab24e4a245ebf0c5cbe53
Max Yaswen <myaswen@gmail.com>
2022-12-01 14:59:32 -0800

    Fix overlapping picker labels on text editor

--- react-app/src/components/PageView/PageView.js
+++ react-app/src/components/PageView/PageView.js
@@ -40,7 +40,6 @@ const PageView = () => {
             setShowDeleteView(false);
             setErrors(response.errors);
         } else {
-            console.log("EDITED PAGE RETURNED: ", response);
             history.push(`/notebooks/${response.notebookId}/pages/${response.id}`);
         }
     }
